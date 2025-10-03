1702adcea8965c7506c086a63d5db36c90bcee18
Heyam <heyambaatwah@gmail.com>
2025-10-03 23:24:11 +0300

    new version of first task

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'المسبحة الإلكترونية',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
       home: const MyHomePage(title: 'المسبحة الإلكترونية'),
     );
   }
@@ -37,60 +33,144 @@ class Adhkar {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Adhkar> adhkar = [Adhkar('سبحان الله'),Adhkar('الحمد الله'),Adhkar('الله اكبر')];
+  List<Adhkar> adhkar = [Adhkar('سُبْحانَ الله'), Adhkar('الحَمْدُ لله'), Adhkar('اللهُ أكْبَر')];
+  int current = 0;
+  int num = 1;
 
-  void _incrementCounter(Adhkar dhikr) {
-    setState(() {
-      dhikr._counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Center(child: Text(widget.title, style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),),
+        backgroundColor: Color(0xff306732),
+        title: Center(
+          child: Text(
+            widget.title,
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.white),
+          ),
+        ),
       ),
-      body: Stack(
-        alignment: Alignment.centerLeft,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+          // progress bar
+          Container(
+            height: 60,
+            color: Color(0xFFD8D8D8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                buildCounter(adhkar[0]),
-                SizedBox(height: 30,),
-                buildCounter(adhkar[1]),
-                SizedBox(height: 30,),
-                buildCounter(adhkar[2])
-              ]
+                Text(
+                  '33/${adhkar[current]._counter}',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(
+                  width: 300,
+                  height: 10,
+                  child: Stack(
+                    alignment: Alignment.centerRight,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      Container(
+                        width: (300/33)*adhkar[current]._counter.toDouble(),
+                        decoration: BoxDecoration(
+                          color: Color(0xff306732),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
-          Positioned(
-            bottom: 30,
-            left: 30,
-            child: ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  for(Adhkar i in adhkar) {
-                    i._counter = 0;
-                  }
-                });
-              },
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-                  foregroundColor: Colors.white,
-                  padding: EdgeInsets.all(20),
-                  shape: CircleBorder()
-              ),
-              child: Text(
-                '×',
+          //text
+          Column(
+            children: [
+
+              Text(
+                adhkar[current].dhikr,
                 style: TextStyle(
-                  fontSize: 40,
+                    fontSize: 60
                 ),
               ),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 30,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      color: current > 1 ? Color(0xff306732) : Colors.grey,
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20), bottomLeft: Radius.circular(20)),
+                    ),
+                  ),
+                  SizedBox(width: 5,),
+                  Container(
+                    height: 30,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      color: current > 0 ? Color(0xff306732) : Colors.grey,
+                    ),
+                  ),
+                  SizedBox(width: 5,),
+                  Container(
+                    height: 30,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      color: current >= 0 ? Color(0xff306732) : Colors.grey,
+                      borderRadius: BorderRadius.only(topRight: Radius.circular(20), bottomRight: Radius.circular(20)),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          //read button
+          Padding(
+            padding: const EdgeInsets.only(bottom: 15),
+            child: ElevatedButton(
+                onPressed: () {
+                      setState(() {
+                        if(adhkar[current]._counter < 33)
+                          adhkar[current]._counter++;
+                        else
+                          {
+                            adhkar[current]._counter = 0;
+                            current= num % 3;
+                            num++;
+                          }
+                      });
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xff306732),
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(horizontal: 170, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)
+                    )
+                ),
+                child: Text(
+                  'قراءة',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
+                )
             ),
           ),
         ],
@@ -98,43 +178,5 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Column buildCounter(Adhkar dhikr) {
-    return Column(
-      children: [
-        Text(
-          dhikr.dhikr,
-          style: TextStyle(
-              fontSize: 40
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 10),
-          child: Text(
-            '${dhikr._counter}',
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-        ),
-        ElevatedButton(
-            onPressed: () {
-              _incrementCounter(dhikr);
-            },
-            style: ElevatedButton.styleFrom(
-                backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-                foregroundColor: Colors.white,
-                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 0),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)
-                )
-            ),
-            child: Text(
-              dhikr.dhikr,
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold
-              ),
-            )
-        ),
-      ],
-    );
-  }
+
 }
