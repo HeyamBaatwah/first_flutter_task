316bf3ade3e03742745920b12811f0399f078a86
Heyam <heyambaatwah@gmail.com>
2025-10-12 14:47:16 +0300

    add to do app

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(),
     );
   }
@@ -76,7 +77,6 @@ void _deleteTask(int index) {
                     fontWeight: FontWeight.bold,
                     color: Colors.pink.shade100,
                   ),
-                  border: InputBorder.none,
                 ),
               ),
             ),
